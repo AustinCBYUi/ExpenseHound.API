a93ad2ea165580f7028046765a2a2e81af8204e8
Austin Campbell <131830841+AustinCBYUi@users.noreply.github.com>
2024-12-18 18:22:26 -0700

    Add project files.

---
+++ ExpenseHound.API/Program.cs
@@ -0,0 +1,39 @@
+using ExpenseHound.API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<ExpenseService>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorClient", 
+        builder => builder.WithOrigins("https://expensehound20241217154831-expensehound.azurewebsites.net/")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowBlazorClient");
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
